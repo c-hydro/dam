76ce605028a5728981ac5984d1d87d29f4de6dc7
ltrotter <lucatrotter@gmail.com>
2024-02-01 16:35:03 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dam',
+    version='0.1',
+    packages=find_packages(),
+    description='A package for raster data processing developed at the CIMA Research Foundation',
+    author='Luca Trotter',
+    author_email='luca.trotter@cimafoundation.org',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ],
+    keywords='meteorological data, satellite data, climatological data, environmental data, raster data,\
+        xarray, netcdf, hdf5, grib, hdf4, hdf-eos, hdf-eos5, geotiff',
+    install_requires=[
+        'gdal[numpy] >= 3.4.3'
+    ],
+    python_requires='>=3.10',
+    test_suite='tests',
+)
