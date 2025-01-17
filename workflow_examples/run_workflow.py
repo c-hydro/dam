78fd5f60de774cb78a69039413f47d7dbc154abe
ltrotter <lucatrotter@gmail.com>
2025-01-17 11:57:34 +0100

    refactor(examples): create run_workflow scripts to revamp examples

---
+++ workflow_examples/run_workflow.py
@@ -0,0 +1,45 @@
+import argparse
+
+from d3tools import Options
+from d3tools.timestepping import get_date_from_str
+
+from dam import DAMWorkflow
+
+def parse_arguments():
+    """
+    Parse command line arguments for the workflow.
+
+    Returns:
+        argparse.Namespace: Parsed command line arguments.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Run workflow with specified parameters",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('workflow_json', type=str, help='JSON file of the workflow')
+    parser.add_argument('-s', '--start', type=str, required=True, help='Start date to run the workflow [YYYY-MM-DD]')
+    parser.add_argument('-e', '--end',   type=str, required=True, help='End date to run the workflow [YYYY-MM-DD]')
+    
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = parse_arguments()
+
+    # load the options from the json file
+    options = Options.load(args.workflow_json)
+
+    # set the start and end date
+    start_date = get_date_from_str(args.start) if args.start else None
+    end_date   = get_date_from_str(args.end)   if args.end   else None
+
+    # create the workflow
+    wf = DAMWorkflow.from_options(options.DAM_WORKFLOW)
+
+    # run the computation
+    wf.run([start_date, end_date])
+    
+if __name__ == '__main__':
+    main()
