6d934e26168d1852d8dd0cf9ad17c8b4162f42d7
ltrotter <lucatrotter@gmail.com>
2025-02-10 16:07:17 +0100

    feat(timeagg): update mean and sum aggregation functions

--- src/dam/processing/time_agg.py
+++ src/dam/processing/time_agg.py
@@ -11,16 +11,14 @@ from typing import Sequence
 def sum(input: list[xr.DataArray],
         input_agg: list[TimeRange],
         this_agg: TimeRange,
+        na_ignore = True,
         **kwargs) -> xr.DataArray:
-     
-     overlaps = calc_overlap(this_agg, input_agg)
-     total_overlap = np.sum(overlaps)
-     rel_overlaps = [ov / total_overlap for ov in overlaps]
 
-     multiplied_data = [rel_ov * da.values * len(input) for rel_ov, da in zip(rel_overlaps, input)]
-     all_inputs = np.stack(multiplied_data, axis = 0)
-     
-     sum_data = np.sum(all_inputs, axis = 0)
+     all_inputs = np.stack(input, axis = 0)
+     if na_ignore:
+          sum_data = np.nansum(all_inputs, axis = 0)
+     else:
+          sum_data = np.sum(all_inputs, axis = 0)
      sum_da = xr.DataArray(sum_data, coords = input[0].coords, dims = input[0].dims)
 
      return sum_da
@@ -29,19 +27,22 @@ def sum(input: list[xr.DataArray],
 def mean(input: list[xr.DataArray],
          input_agg: list[TimeRange],
          this_agg: TimeRange,
+         na_ignore = True,
          **kwargs) -> xr.DataArray:
      
-     overlaps = calc_overlap(this_agg, input_agg)
-     total_overlap = np.sum(overlaps)
-     rel_overlaps = [ov / total_overlap for ov in overlaps]
+     all_inputs = np.stack(input, axis = 0)
 
-     multiplied_data = [rel_ov * da.values for rel_ov, da in zip(rel_overlaps, input)]
-     all_inputs = np.stack(multiplied_data, axis = 0)
+     if na_ignore:
+          sum_data = np.nansum(all_inputs, axis = 0)
+          count_data = np.sum(~np.isnan(all_inputs), axis = 0)
+          mean_data = sum_data / count_data
+     else:
+          sum_data = np.sum(all_inputs, axis = 0)
+          mean_data = sum_data / len(input)
+     
+     mean_da = xr.DataArray(mean_data, coords = input[0].coords, dims = input[0].dims)
 
-     sum_data = np.sum(all_inputs, axis = 0)
-     sum_da = xr.DataArray(sum_data, coords = input[0].coords, dims = input[0].dims)
-
-     return sum_da
+     return mean_da
 
 def calc_overlap(this_tr: TimeRange, other_tr: TimeRange|list[TimeRange]) -> TimeRange:
      if isinstance(other_tr, Sequence):
@@ -56,5 +57,3 @@ def calc_overlap(this_tr: TimeRange, other_tr: TimeRange|list[TimeRange]) -> Tim
           original_size = this_tr.length(unit = 'hours')
 
      return size / original_size
-
-     
